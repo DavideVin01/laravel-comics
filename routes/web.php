a04fb59e83acdc02b7ef02e28e52fa4e76b5d737
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-15 16:11:49 +0100

    Creates routes + imports bootstrap

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+});
+Route::get('/comics', function () {
+    return 'Comics';
+});
+Route::get('/characters', function () {
+    return 'Characters';
+});
+Route::get('/movies', function () {
+    return 'movies';
+});
+Route::get('/tv', function () {
+    return 'tv';
+});
+Route::get('/games', function () {
+    return 'games';
+});
+Route::get('/collectibles', function () {
+    return 'collectibles';
+});
+Route::get('/videos', function () {
+    return 'videos';
+});
+Route::get('/fans', function () {
+    return 'fans';
+});
+Route::get('/news', function () {
+    return 'news';
+});
+Route::get('/shop', function () {
+    return 'shop';
 });
