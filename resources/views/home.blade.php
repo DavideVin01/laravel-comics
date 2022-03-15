7fa5b8ab4ba5ce84e1a2da6cca0c75ebf86fe97b
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-15 17:23:02 +0100

    Adds vars + links routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,51 @@
 
 <body>
     <header>
-        <h1 class="text-success">Home</h1>
+        <div class="bg-white shadow">
+            <div class="container">
+                <header>
+                    <img id="header-logo" src="../assets/dc-logo.png" alt="DC LOGO" />
+                    <ul>
+                        <li>
+                            <a href="{{ route('characters') }}">Characters</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('comics') }}">Comics</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('movies') }}">Movies</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('tv') }}">Tv</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('games') }}">Games</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('collectibles') }}">Collectibles</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('videos') }}">Videos</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('fans') }}">Fans</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('news') }}">News</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('shop') }}">Shop</a>
+                        </li>
+                    </ul>
+                </header>
+            </div>
+        </div>
     </header>
+    <section>
+        <div id="content" class="bg-black">
+            <div class="container"></div>
+        </div>
+    </section>
     <main></main>
     <footer></footer>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -16,33 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Home');
 });
+
 Route::get('/comics', function () {
     return view('Comics');
-});
+})->name('comics');
+
 Route::get('/characters', function () {
     return view('Characters');
-});
+})->name('characters');
+
 Route::get('/movies', function () {
     return view('movies');
-});
+})->name('movies');
+
 Route::get('/tv', function () {
     return view('tv');
-});
+})->name('tv');
+
 Route::get('/games', function () {
     return view('games');
-});
+})->name('games');
+
 Route::get('/collectibles', function () {
     return view('collectibles');
-});
+})->name('collectibles');
+
 Route::get('/videos', function () {
     return view('videos');
-});
+})->name('videos');
+
 Route::get('/fans', function () {
     return view('fans');
-});
+})->name('fans');
+
 Route::get('/news', function () {
     return view('news');
-});
+})->name('news');
+
 Route::get('/shop', function () {
     return view('shop');
-});
+})->name('shop');
