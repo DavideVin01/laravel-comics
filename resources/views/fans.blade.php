ead5263c32d4be967bc29c833e8992069963c4e8
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-15 16:55:16 +0100

    Creates Views + fixes bootstrap

--- resources/views/fans.blade.php
+++ resources/views/fans.blade.php
@@ -6,15 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap-grid.css'
-        integrity='sha512-pM/iUb80UpXwRGB2/UbpFDyPtO31IE5aokTh7sYjpSY06pH3j0hXCNXGkyRn8gVod9Cbo4GdP/n98rfu6JC75Q=='
-        crossorigin='anonymous' />
-    <title>DC COMICS | Home</title>
+    <title>DC COMICS | Fans</title>
 </head>
 
 <body>
     <header>
-        <h1 class="text-success">Home</h1>
+        <h1>Fans</h1>
     </header>
     <main></main>
     <footer></footer>
