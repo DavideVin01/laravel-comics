7f6ea0ff84d9977e9e0ce7df162ae1f1f1bea1ab
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-16 19:39:57 +0100

    Imports Fontawesome

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -10,6 +10,9 @@
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap-grid.css'
         integrity='sha512-pM/iUb80UpXwRGB2/UbpFDyPtO31IE5aokTh7sYjpSY06pH3j0hXCNXGkyRn8gVod9Cbo4GdP/n98rfu6JC75Q=='
         crossorigin='anonymous' />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css"
+        integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>DC COMICS | Home</title>
 </head>
 
