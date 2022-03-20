ec4fb332bf97c789af611549c877cdb99bdae586
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-20 22:20:54 +0100

    Fixes css

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,25 +3,28 @@
 
 
 @section('main')
+    <section>
+        <div class="box-blue"></div>
+    </section>
     <div class="container">
-        <section>
-            {{-- Blue Section --}}
-        </section>
-        <section>
-            <img class="mb-5" src="{{ $comic['thumb'] }}" alt="">
-            <h2 class="text-uppercase">{{ $comic['title'] }}</h2>
-            <div class="box-green">
-                <div id="comic-price">{{ $comic['price'] }}</div>
-            </div>
-            <div class="d-flex">
+        <div class="row">
+
+            <div class="col-8">
+                <img class="mb-5" src="{{ $comic['thumb'] }}" alt="">
+                <h2 class="text-uppercase">{{ $comic['title'] }}</h2>
+                <div class="box-green p-1 my-3">
+                    <div id="comic-price" class="text-white"><strong>Comic Price:</strong> {{ $comic['price'] }}</div>
+                </div>
                 <div id="comic-description">{{ $comic['description'] }}</div>
+            </div>
+            <div class="col-4 d-flex align-items-end">
                 <div class="ms-5">
-                    <div>ADVERTISEMENT</div>
+                    <div class="d-flex justify-content-end">ADVERTISEMENT</div>
                     <div id="adv-img">
                         <img src="{{ asset('/img/advertise.jpg') }}" alt="ADV">
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     </div>
 @endsection
